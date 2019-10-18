ca4244f7bd378c4b2269226b0293ee9cc8e8377b
李超逸 <>
2019-10-19 05:14:43 +0900

    add spacer and padding

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -14,22 +14,23 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
+            Spacer()
             // Target row
             HStack {
                 Text("Drag the slider as close as you can to:")
                 Text("100")
             }
-            
+            Spacer()
             // Slider row
             HStack {
                 Text("1")
                 Slider(value: .constant(100))
                 Text("100")
             }
-
+            Spacer()
             // Button row
             VStack {
-
+                
                 Button(action: {
                     self.alertIsVisable = true
                 }) {
@@ -41,20 +42,24 @@ struct ContentView: View {
                                  dismissButton: .default(Text("Awesome!")))
                 }
             }
-            
+            Spacer()
             // Score row
             HStack {
                 Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
                     Text("Start Over")
                 }
+                Spacer()
                 Text("Score:")
                 Text("99999")
+                Spacer()
                 Text("Round:")
                 Text("999")
+                Spacer()
                 Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
                     Text("Info")
                 }
             }
+            .padding(.bottom, 20)
         }
     }
 }
